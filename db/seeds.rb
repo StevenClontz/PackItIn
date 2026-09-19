e3cdf7a3eb1d1006cdb6a2b4fd904af01f379030
Steven Clontz <steven.clontz@gmail.com>
2026-09-19 02:34:36 +0000

    fix dev, add example in dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Rails.env.development?
+  # Devise downcases usernames on save, so look up via find_for_authentication
+  # (which does the same) to keep this idempotent.
+  Family.find_for_authentication(username: "ExampleFamily") ||
+    Family.create!(username: "ExampleFamily", password: "password123")
+end
